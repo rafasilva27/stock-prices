e528b6c400bf412f46c0fcb485769a60f49e18ef
Rafaela Silva <rafalourenco.s@hotmail.com>
2024-09-24 14:24:24 -0300

    add requirements file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # app resultado de ações
+import math
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 from datetime import timedelta
-st.write("""
-# App Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo dos anos
-""")
+
+st.write("""# App Preço de Ações""")
 
 @st.cache_data # pra armazenar os dados em cache
 def carregar_dados(empresas):
@@ -16,7 +15,14 @@ def carregar_dados(empresas):
     cotacoes_acao = cotacoes_acao["Close"]
     return cotacoes_acao
 
-acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
+@st.cache_data
+def carregar_tickers_acoes():
+    base_tickers = pd.read_csv("IBOV.csv", sep=";")
+    tickers = list(base_tickers["Código"]) # pegando apenas a coluna Código
+    tickers = [item + ".SA" for item in tickers] # adicionando .SA no final de cada ticker
+    return tickers
+
+acoes = carregar_tickers_acoes()
 dados  = carregar_dados(acoes) 
 
 st.sidebar.header("Filtros")
@@ -34,7 +40,25 @@ data_inicial = dados.index.min().to_pydatetime()
 data_final = dados.index.max().to_pydatetime()
 intervalo_data = st.sidebar.slider("Selecione o periodo:", min_value=data_inicial, max_value=data_final, value=(data_inicial, data_final), step=timedelta(days=1))
 
-dados = dados.loc[intervalo_data[0]:intervalo_data[1]] #filtrando as linhas que correspondem ao intervalo de datas selecionado pelo usuário        
+dados = dados.loc[intervalo_data[0]:intervalo_data[1]] # filtrando as linhas que correspondem ao intervalo de datas selecionado pelo usuário        
         
-st.line_chart(dados)
+st.line_chart(dados) # criando o gráfico
 
+texto_performance_ativos = " "
+
+if len(lista_acoes) == 0:
+    lista_acoes = list(dados.columns)
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns={"Close": acao_unica})
+elif math.isnan(performance_ativo):
+    texto_performance_ativos = texto_performance_ativos +  f"não foi possiível calcular a performance"    
+
+for acao in lista_acoes:
+    performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1 # calculando a performance de quantos % cada ativo subiu ou caiu no período, valor final / valor inicial - 1
+    performance_ativo = float(performance_ativo)
+    texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: {performance_ativo:.1%}"
+
+st.write(f"""
+Essa foi a performance de cada ativo no período selecionado:
+   {texto_performance_ativos}     
+""")
